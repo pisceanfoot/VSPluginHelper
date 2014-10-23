592f0bb2b808c0825fb8f8dc815148f2df176d03
leo.hecl <leo.hecl@ef.com>
2014-10-23 15:14:28 +0800

    change to static

--- VSPluginHelper/VSPluginHelper/PluginManager.cs
+++ VSPluginHelper/VSPluginHelper/PluginManager.cs
@@ -10,19 +10,34 @@ namespace VSPluginHelper
 {
     public class PluginManager
     {
+        private static PluginManager pluginManager;
+
         private DTE2 _applicationObject;
         private AddIn _addInInstance;
 
         private MenuManager menuManager;
         private WindowManager windowManager;
 
-        public PluginManager(DTE2 _applicationObject, AddIn _addInInstance)
+        private PluginManager(DTE2 _applicationObject, AddIn _addInInstance)
         {
             this._applicationObject =_applicationObject;
             this._addInInstance = _addInInstance;
 
             menuManager = new MenuManager(_applicationObject);
             windowManager = new WindowManager(_applicationObject, _addInInstance);
+        }
+
+        public static void Init(DTE2 _applicationObject, AddIn _addInInstance)
+        {
+            pluginManager = new PluginManager(_applicationObject, _addInInstance);
+        }
+
+        public static PluginManager Current
+        {
+            get
+            {
+                return pluginManager;
+            }
         }
 
         public MenuManager MenuManager 
